6e07d65a267d003bca516b99e40b9725556b997b
unknown <danibertora@hotmail.com>
2022-06-06 01:24:35 -0300

    modifique el head, header y otros detalles minimos, verificar los ejs de cada uno

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const { application } = require("express");
 const express = require("express");
 const path = require("path");
 
-
 const app = express();
-
+const methodOverride=require("method-override");
+app.use(methodOverride("_method"));
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
-
-app.set("view engine", "ejs")
-
 
 
 app.listen(3000, () => {
   console.log("Servidor corriendo en puerto 3000");
 });
-
-const mainroute = require("./route/mainroute");
-
-
-
-app.use("/",mainroute)
+app.use(express.urlencoded ({extented:false}));
+app.use(express.json());
+app.set("view engine", "ejs");
+const mainRoutes = require("./routes/main") ;
+app.use("/", mainRoutes );
+app.use((req,res) => {
+  res.status(404).render("not-found")
+}
+)
 
 
 
