eea074f301c50bded183094729967a1d76db2f9d
J <A@A.COM>
2022-06-20 17:09:16 -0300

    a

--- app.js
+++ app.js
@@ -3,28 +3,19 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const methodOverride=require("method-override");
+const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-
 app.listen(3000, () => {
   console.log("Servidor corriendo en puerto 3000");
 });
-app.use(express.urlencoded ({extented:false}));
+app.use(express.urlencoded({ extented: false }));
 app.use(express.json());
 app.set("view engine", "ejs");
-const mainRoutes = require("./routes/main") ;
-app.use("/", mainRoutes );
-app.use((req,res) => {
-  res.status(404).render("not-found")
-}
-)
-
-
-
-
-
-
-
+const mainRoutes = require("./routes/main");
+app.use("/", mainRoutes);
+app.use((req, res) => {
+  res.status(404).render("not-found");
+});
