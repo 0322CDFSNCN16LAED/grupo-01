7153990a4cba329d41d610e7b981460c371a6d76
J <A@A.COM>
2022-09-10 19:35:12 -0300

    arreglando vistas

--- controller/maincontroler.js
+++ controller/maincontroler.js
@@ -1,289 +1,203 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
 const { redirect } = require("express/lib/response.js");
-const fs = require ("fs");
-const path = require ("path");
-const db = require ("../database/db.js");
-const bcryptjs = require ('bcryptjs');
-const dbp = require("../database/models")
-const { v4 } = require("uuid")
-const uuid = v4
+const fs = require("fs");
+const path = require("path");
+const db = require("../database/db.js");
+const bcryptjs = require("bcryptjs");
+const dbp = require("../database/models");
+const { v4 } = require("uuid");
+const uuid = v4;
 
-
-
-const productos = db.getAll() ;
-const users = db.getAllUsers() ;
-
+const productos = db.getAll();
+const users = db.getAllUsers();
 
 const controller = {
-    home : (req,res)=>{
-        dbp.Productos.findAll()
-        .then(function(productos){
-            res.render("home", {productos : productos})
-        })
-    },
-    
-    login : (req,res)=> {
-        
-        res.render("login")
-    },
-    processlogin : (req,res)=>{
-        const users = db.getAllUsers()
-        const email = req.body.email;
-        const password = req.body.Password;
+  home: (req, res) => {
+    dbp.Productos.findAll().then(function (productos) {
+      res.render("home", { productos: productos });
+    });
+  },
 
-        userFind = users.find(user =>user.correo == email);
+  login: (req, res) => {
+    res.render("login");
+  },
+  help: (req, res) => {
+    res.render("ayuda");
+  },
+  processlogin: (req, res) => {
+    const users = db.getAllUsers();
+    const email = req.body.email;
+    const password = req.body.Password;
 
-        let errors = validationResult(req)
+    userFind = users.find((user) => user.correo == email);
 
-        if(errors.isEmpty){
+    let errors = validationResult(req);
 
-        if(userFind) {
-            if (bcryptjs.compareSync(password ,userFind.contrasena)){
+    if (errors.isEmpty) {
+      if (userFind) {
+        if (bcryptjs.compareSync(password, userFind.contrasena)) {
+          req.session.usuarioLogueado = userFind;
 
-                req.session.usuarioLogueado = userFind;
+          if (req.body.recordame != undefined) {
+            res.cookie("recordame", req.body.email, { maxAge: 60000 });
+          }
 
-
-                if (req.body.recordame != undefined) {
-                    res.cookie('recordame', req.body.email, { maxAge: 60000 })
-                }
-
-                res.redirect("/")
-            }else {
-                res.render("login", { errors : { password : { msg : "credenciales invalidas"}}})
-            }
-            }else {
-                res.render("login", { errors : { email : { msg : "usuario inexistente"}}})
-            }
-    }
-
-
-
-    },
-      
-    listarProductos : (req,res)=>{
-        dbp.Productos.findAll({include : [{association : "reemplaza"}]})
-        .then(function(productos){ 
-            res.render("listaProductos", {productos : productos})
-        })
-    
-    },
-
-    detalleproducto : (req,res)=>{
-        let id = req.params.id ;
-        dbp.Productos.findByPk(id,{
-            include : [{association : "reemplaza"}]
-        })
-        .then((productoDetalle)=>{
-            res.render("detalleproducto", {productoDetalle : productoDetalle})
-        })
- 
-    },
-
-    carrito : (req,res)=>{
-        
-        res.render("carrito", {productoDetalle : productoDetalle})
-    },
-
-    addCarrito : (req,res)=>{
-        let id = req.params.id ;
-        let productoDetalle= db.getOne(id); 
-        
-        res.render("carrito", {productoDetalle : productoDetalle})
-    },
-    busqueda : (req,res) => {
-        
-    },
-    createProducto : (req,res) => {
-        dbp.Reemplaza.findAll()
-        .then((reemplaza)=>{
-            res.render("create-form-products", { reemplaza : reemplaza})
-        })
-        
-    },
-
- 
-   guardarProducto : (req,res) => {
-    
-    if(req.file){
-        bp.Productos.create({
-            ...req.body,
-            imagen : req.file.filename,
-            })
-            then((producto)=>{
-            res.redirect("/listaProductos") ;
-        })   
-
-    }else{
-        
-
-        dbp.Reemplaza.findAll()
-        .then((reemplaza)=>{
-            res.render("create-form-products", { reemplaza : reemplaza})
-        })
-        }  
-    }
-,
-        editarProducto: (req,res) => {
-        
-        Promise.all([
-            dbp.Productos.findByPk(req.params.id),
-            dbp.Reemplaza.findAll(),
-
-            
-
-
-        ]).then(function([productToEdit, reemplaza]){
-            console.log(reemplaza)
-            res.render("editarProducto", {productToEdit : productToEdit, reemplaza : reemplaza});
-        });
-            
-       
-        
-    },
-    editProducto: (req,res) => {
-        
-        dbp.Productos.findByPk(req.params.id).then((producto)=>{
-            producto.set(req.body);
-            if (req.file){
-
-                producto.imagen = req.file.filename
-            }
-
-            producto.save().then(() =>{
-                res.redirect("/listaProductos");
-            });
-
-
-        });
-        
-   
-        
-       
-    },
-
-
-    eliminarProducto: (req,res) => {
-
-
-
-        dbp.Productos.findByPk(req.params.id).then((producto)=>{
-
-           
-
-                producto.destroy().then(()=>{
-
-                    res.redirect("/listaProductos");
-
-                })
-
-            
-
-        })  
-
-    },
-
-    listarUsuario : (req,res)=>{
-        dbp.Usuarios.findAll()
-        .then((users)=>{
-            res.render("usersList", {users : users})
-        })
-       
-    },
-
-    detalleUsuario : (req,res)=>{
-        let id = req.params.id ;
-        dbp.Usuarios.findByPk(id)
-        .then((usuarioDetalle)=>{
-            res.render("detalleusuario", {usuarioDetalle : usuarioDetalle})
-        })
-        
-        
-    },
-
-
-
-    createUsuario : (req,res) => {
-
-        
-        res.render("register")
-
-    },
-
-    guardarUsuario : (req,res) => {
-        
-        let errors = validationResult(req);
-        if (errors.isEmpty()){
-
-        
-
-
-            if(req.file){
-                
-                dbp.Usuarios.create({
-                    ...req.body,
-                    imagen : req.file.filename,
-                    contraseña : req.body.contrasena
-                   
-
-                })
-                
-
-                res.redirect("/") ;
-            }
-
-        }else{
-
-            res.render('register', {errors : errors.mapped(0)});
+          res.redirect("/");
+        } else {
+          res.render("login", {
+            errors: { password: { msg: "credenciales invalidas" } },
+          });
         }
-} ,
+      } else {
+        res.render("login", {
+          errors: { email: { msg: "usuario inexistente" } },
+        });
+      }
+    }
+  },
 
-eliminarUsuario : (req,res)=>{
-    const users = db.getAllUsers()
-    const id = req.params.id
-    usersFilter = users.filter((user)=> user.id != id);
-    db.writeAndSaveUser(usersFilter); 
+  listarProductos: (req, res) => {
+    dbp.Productos.findAll({ include: [{ association: "reemplaza" }] }).then(
+      function (productos) {
+        res.render("listaProductos", { productos: productos });
+      }
+    );
+  },
+
+  detalleproducto: (req, res) => {
+    let id = req.params.id;
+    dbp.Productos.findByPk(id, {
+      include: [{ association: "reemplaza" }],
+    }).then((productoDetalle) => {
+      res.render("detalleproducto", { productoDetalle: productoDetalle });
+    });
+  },
+
+  carrito: (req, res) => {
+    res.render("carrito", { productoDetalle: productoDetalle });
+  },
+
+  addCarrito: (req, res) => {
+    let id = req.params.id;
+    let productoDetalle = db.getOne(id);
+
+    res.render("carrito", { productoDetalle: productoDetalle });
+  },
+  busqueda: (req, res) => {},
+  createProducto: (req, res) => {
+    dbp.Reemplaza.findAll().then((reemplaza) => {
+      res.render("create-form-products", { reemplaza: reemplaza });
+    });
+  },
+
+  guardarProducto: (req, res) => {
+    if (req.file) {
+      bp.Productos.create({
+        ...req.body,
+        imagen: req.file.filename,
+      });
+      then((producto) => {
+        res.redirect("/listaProductos");
+      });
+    } else {
+      dbp.Reemplaza.findAll().then((reemplaza) => {
+        res.render("create-form-products", { reemplaza: reemplaza });
+      });
+    }
+  },
+  editarProducto: (req, res) => {
+    Promise.all([
+      dbp.Productos.findByPk(req.params.id),
+      dbp.Reemplaza.findAll(),
+    ]).then(function ([productToEdit, reemplaza]) {
+      console.log(reemplaza);
+      res.render("editarProducto", {
+        productToEdit: productToEdit,
+        reemplaza: reemplaza,
+      });
+    });
+  },
+  editProducto: (req, res) => {
+    dbp.Productos.findByPk(req.params.id).then((producto) => {
+      producto.set(req.body);
+      if (req.file) {
+        producto.imagen = req.file.filename;
+      }
+
+      producto.save().then(() => {
+        res.redirect("/listaProductos");
+      });
+    });
+  },
+
+  eliminarProducto: (req, res) => {
+    dbp.Productos.findByPk(req.params.id).then((producto) => {
+      producto.destroy().then(() => {
+        res.redirect("/listaProductos");
+      });
+    });
+  },
+
+  listarUsuario: (req, res) => {
+    dbp.Usuarios.findAll().then((users) => {
+      res.render("usersList", { users: users });
+    });
+  },
+
+  detalleUsuario: (req, res) => {
+    let id = req.params.id;
+    dbp.Usuarios.findByPk(id).then((usuarioDetalle) => {
+      res.render("detalleusuario", { usuarioDetalle: usuarioDetalle });
+    });
+  },
+
+  createUsuario: (req, res) => {
+    res.render("register");
+  },
+
+  guardarUsuario: (req, res) => {
+    let errors = validationResult(req);
+    if (errors.isEmpty()) {
+      if (req.file) {
+        dbp.Usuarios.create({
+          ...req.body,
+          imagen: req.file.filename,
+          contraseña: req.body.contrasena,
+        });
+
+        res.redirect("/");
+      }
+    } else {
+      res.render("register", { errors: errors.mapped(0) });
+    }
+  },
+
+  eliminarUsuario: (req, res) => {
+    const users = db.getAllUsers();
+    const id = req.params.id;
+    usersFilter = users.filter((user) => user.id != id);
+    db.writeAndSaveUser(usersFilter);
     res.redirect("/userList");
-    
+  },
+  editUser: (req, res) => {
+    let id = req.params.id;
+    dbp.Usuarios.findByPk(id).then((userToEdit) => {
+      res.render("editUser", { userToEdit: userToEdit });
+    });
+  },
+  userEdited: (req, res) => {
+    dbp.Usuarios.update(
+      {
+        ...req.body,
+      },
+      {
+        where: { idUsuario: req.params.id },
+      }
+    ).then((usuarioEdited) => {
+      res.redirect("/userList");
+    });
+  },
+};
 
-
-},
-editUser : (req,res) => {
-    
-    let id = req.params.id ;
-    dbp.Usuarios.findByPk(id)
-    .then((userToEdit)=>{
-        res.render("editUser", {userToEdit : userToEdit});
-    })
-    
-    
-    
-},
-userEdited : (req,res) => {
-    dbp.Usuarios.update({
-        ...req.body   
-    }, {
-        where : {idUsuario : req.params.id}
-    })
-    .then((usuarioEdited)=>{
-        res.redirect("/userList");
-    })
-    
-
-
-     
-}
-
-
-}
-
-
-   
-
-
-
-
-
-module.exports = controller
-
-
+module.exports = controller;
--- routes/main.js
+++ routes/main.js
@@ -1,97 +1,96 @@
 const express = require("express");
-const multer = require("multer")
-const path = require("path")
+const multer = require("multer");
+const path = require("path");
 const { append } = require("express/lib/response");
 const { home } = require("../controller/maincontroler");
-
-
 
 const router = express.Router();
 
 const controller = require("../controller/maincontroler");
-const {check, body, validationResult} = require('express-validator');
+const { check, body, validationResult } = require("express-validator");
 const authMiddleware = require("../middlewares/authMiddleware");
 const guestMiddleware = require("../middlewares/guestMiddleware");
-const masterMiddleware = require ("../middlewares/masterMiddleware")
-
+const masterMiddleware = require("../middlewares/masterMiddleware");
 
 const validateRegister = [
-    body('nombre').notEmpty().withMessage('Debes completar el campo nombre') 
-    .isLength({min: 2}).withMessage('Debe tener al menos dos caracteres') 
-    ,
-    body('apellido').notEmpty() .withMessage('Debes completar el campo apellido'),
-    body("nacimiento").notEmpty().withMessage("Debe completar el campo nacimiento"),
-    body('sexo').notEmpty().withMessage('Debes completar el campo sexo'),
-    body('celular').notEmpty() .withMessage('Debes completar el campo celular'),
-    
-    body('email').isEmail().withMessage("Debes completar un correo válido"),
-    body('contrasena')
-    .notEmpty().withMessage("Debes completar el campo contraseña ")
-    .isLength({min: 8}).withMessage('Debe tener al menos ocho caracteres') 
-    //.matches(/^(?=.\d)(?=.[!@#$%^&])(?=.[a-z])(?=.*[A-Z]).{8,}$/).withMessage('Debe contener ciertos caracteres') 
-    ,
-    body("categoriaInteres").notEmpty().withMessage("Debes seleccionar al menos una opción"),
-    //body("password").custom((value, { req }) => {
-      //  if (value !== req.body.password) {
-      //    throw new Error("Las contraseñas no coincidennnnn");
-      // } else {
-       //   return true;
-       // }
-      //}),
-]
-const validateLogin = [
-    check('email').isEmail().withMessage ('El email no es válido'),
-    check ('password').isLength({min: 8}).withMessage ('La contraseña debe tener al menos 8 carácteres')
-]
+  body("nombre")
+    .notEmpty()
+    .withMessage("Debes completar el campo nombre")
+    .isLength({ min: 2 })
+    .withMessage("Debe tener al menos dos caracteres"),
+  body("apellido").notEmpty().withMessage("Debes completar el campo apellido"),
+  body("nacimiento")
+    .notEmpty()
+    .withMessage("Debe completar el campo nacimiento"),
+  body("sexo").notEmpty().withMessage("Debes completar el campo sexo"),
+  body("celular").notEmpty().withMessage("Debes completar el campo celular"),
 
+  body("email").isEmail().withMessage("Debes completar un correo válido"),
+  body("contrasena")
+    .notEmpty()
+    .withMessage("Debes completar el campo contraseña ")
+    .isLength({ min: 8 })
+    .withMessage("Debe tener al menos ocho caracteres"),
+  //.matches(/^(?=.\d)(?=.[!@#$%^&])(?=.[a-z])(?=.*[A-Z]).{8,}$/).withMessage('Debe contener ciertos caracteres')
+  body("categoriaInteres")
+    .notEmpty()
+    .withMessage("Debes seleccionar al menos una opción"),
+  //body("password").custom((value, { req }) => {
+  //  if (value !== req.body.password) {
+  //    throw new Error("Las contraseñas no coincidennnnn");
+  // } else {
+  //   return true;
+  // }
+  //}),
+];
+const validateLogin = [
+  check("email").isEmail().withMessage("El email no es válido"),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña debe tener al menos 8 carácteres"),
+];
 
 const validaterProducter = [
-    body('nombre').notEmpty().withMessage('Debes completar este campo')
-    .isLength({min:5}) .withMessage('Debe tener al menos 5 caracteres') ,
-    body('descrpcion').isLength() .withMessage('Debe tener al menos 20 caracteres'),
-    
-]
-
-
+  body("nombre")
+    .notEmpty()
+    .withMessage("Debes completar este campo")
+    .isLength({ min: 5 })
+    .withMessage("Debe tener al menos 5 caracteres"),
+  body("descrpcion")
+    .isLength()
+    .withMessage("Debe tener al menos 20 caracteres"),
+];
 
 const userStorage = multer.diskStorage({
-    destination : (req, file, callback) => {
-        callback(null, path.join(__dirname, "../public/images/imageUsers") );
-
-    },
-    filename : (req, file, callback)=> {
-        const newName = Date.now () + path.extname(file.originalname);
-        callback(null, newName );
-
-    }
+  destination: (req, file, callback) => {
+    callback(null, path.join(__dirname, "../public/images/imageUsers"));
+  },
+  filename: (req, file, callback) => {
+    const newName = Date.now() + path.extname(file.originalname);
+    callback(null, newName);
+  },
 });
 
-const upload = multer({storage : userStorage });
+const upload = multer({ storage: userStorage });
 
 const productStorage = multer.diskStorage({
-    destination : (req, file, callback) => {
-        callback(null, path.join(__dirname, "../public/images") );
-
-    },
-    filename : (req, file, callback)=> {
-        const newName = Date.now () + path.extname(file.originalname);
-        callback(null, newName );
-
-    }
+  destination: (req, file, callback) => {
+    callback(null, path.join(__dirname, "../public/images"));
+  },
+  filename: (req, file, callback) => {
+    const newName = Date.now() + path.extname(file.originalname);
+    callback(null, newName);
+  },
 });
 
-const productupload = multer({storage : productStorage });
-
-
-
-
-
+const productupload = multer({ storage: productStorage });
 
 router.get("/", controller.home);
-
+router.get("/help", controller.help);
+router.get("/contacto", controller.help);
 //proceso de login
 router.get("/login", guestMiddleware, controller.login);
-router.post('/login',validateLogin, controller.processlogin);
+router.post("/login", validateLogin, controller.processlogin);
 
 router.get("/detalleproducto/:id", controller.detalleproducto);
 router.get("/carrito", controller.carrito);
@@ -100,34 +99,39 @@ router.get("/listaProductos", controller.listarProductos);
 router.get("/busqueda", controller.busqueda);
 
 router.get("/editarProducto/:id", masterMiddleware, controller.editarProducto);
-router.put("/editarProducto/:id",productupload.single("imagen"), controller.editProducto);
+router.put(
+  "/editarProducto/:id",
+  productupload.single("imagen"),
+  controller.editProducto
+);
 
 router.get("/crearProducto", masterMiddleware, controller.createProducto);
-router.post("/crearProducto", productupload.single("imagen"), validaterProducter, controller.guardarProducto);
+router.post(
+  "/crearProducto",
+  productupload.single("imagen"),
+  validaterProducter,
+  controller.guardarProducto
+);
 router.post("/carrito", controller.carrito);
 router.post("/carrito/:id", controller.addCarrito);
 router.delete("/eliminarProducto/:id", controller.eliminarProducto);
-
-
 
 router.get("/userList", masterMiddleware, controller.listarUsuario);
 
 //perfil de usuario
 router.get("/detalleUser/:id", authMiddleware, controller.detalleUsuario);
 
-
 router.get("/crearUsuario", guestMiddleware, controller.createUsuario);
-router.post("/crearUsuario", upload.single("imagenusuario"), validateRegister, controller.guardarUsuario);
+router.post(
+  "/crearUsuario",
+  upload.single("imagenusuario"),
+  validateRegister,
+  controller.guardarUsuario
+);
 router.get("/editUser/:id", authMiddleware, controller.editUser);
-router.put("/editUser/:id",upload.single("imagen"), controller.userEdited);
+router.put("/editUser/:id", upload.single("imagen"), controller.userEdited);
 router.delete("/eliminarUsuario/:id", controller.eliminarUsuario);
 
-router.get('/', controller.login)
+router.get("/", controller.login);
 
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
