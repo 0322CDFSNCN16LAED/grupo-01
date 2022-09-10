ecaa2d524f913308a8452131beee2c1d9e39fd50
J <A@A.COM>
2022-09-10 20:15:49 -0300

    maincontroller

--- controller/maincontroler.js
+++ controller/maincontroler.js
@@ -57,64 +57,147 @@ const controller = {
     }
   },
 
-eliminarUsuario : (req,res)=>{
-    dbp.Usuarios.findByPk(req.params.id).then((usuario)=>{
+  listarProductos: (req, res) => {
+    dbp.Productos.findAll({ include: [{ association: "reemplaza" }] }).then(
+      function (productos) {
+        res.render("listaProductos", { productos: productos });
+      }
+    );
+  },
 
-           
+  detalleproducto: (req, res) => {
+    let id = req.params.id;
+    dbp.Productos.findByPk(id, {
+      include: [{ association: "reemplaza" }],
+    }).then((productoDetalle) => {
+      res.render("detalleproducto", { productoDetalle: productoDetalle });
+    });
+  },
 
-        usuario.destroy().then(()=>{
+  carrito: (req, res) => {
+    res.render("carrito", { productoDetalle: productoDetalle });
+  },
 
-            res.redirect("/userList");
+  addCarrito: (req, res) => {
+    let id = req.params.id;
+    let productoDetalle = db.getOne(id);
 
-        })
+    res.render("carrito", { productoDetalle: productoDetalle });
+  },
+  busqueda: (req, res) => {},
+  createProducto: (req, res) => {
+    dbp.Reemplaza.findAll().then((reemplaza) => {
+      res.render("create-form-products", { reemplaza: reemplaza });
+    });
+  },
 
-    
+  guardarProducto: (req, res) => {
+    if (req.file) {
+      bp.Productos.create({
+        ...req.body,
+        imagen: req.file.filename,
+      });
+      then((producto) => {
+        res.redirect("/listaProductos");
+      });
+    } else {
+      dbp.Reemplaza.findAll().then((reemplaza) => {
+        res.render("create-form-products", { reemplaza: reemplaza });
+      });
+    }
+  },
+  editarProducto: (req, res) => {
+    Promise.all([
+      dbp.Productos.findByPk(req.params.id),
+      dbp.Reemplaza.findAll(),
+    ]).then(function ([productToEdit, reemplaza]) {
+      console.log(reemplaza);
+      res.render("editarProducto", {
+        productToEdit: productToEdit,
+        reemplaza: reemplaza,
+      });
+    });
+  },
+  editProducto: (req, res) => {
+    dbp.Productos.findByPk(req.params.id).then((producto) => {
+      producto.set(req.body);
+      if (req.file) {
+        producto.imagen = req.file.filename;
+      }
 
-})  
+      producto.save().then(() => {
+        res.redirect("/listaProductos");
+      });
+    });
+  },
 
-    
-    
+  eliminarProducto: (req, res) => {
+    dbp.Productos.findByPk(req.params.id).then((producto) => {
+      producto.destroy().then(() => {
+        res.redirect("/listaProductos");
+      });
+    });
+  },
 
+  listarUsuario: (req, res) => {
+    dbp.Usuarios.findAll().then((users) => {
+      res.render("usersList", { users: users });
+    });
+  },
 
-},
-editUser : (req,res) => {
-    
-    let id = req.params.id ;
-    dbp.Usuarios.findByPk(id)
-    .then((userToEdit)=>{
-        res.render("editUser", {userToEdit : userToEdit});
-    })
-    
-    
-    
-},
-userEdited : (req,res) => {
-    dbp.Usuarios.update({
-        ...req.body   ,
-        contraseña : req.body.contrasena
-    }, {
-        where : {idUsuario : req.params.id}
-    })
-    .then((usuarioEdited)=>{
-        res.redirect("/userList");
-    })
-    
+  detalleUsuario: (req, res) => {
+    let id = req.params.id;
+    dbp.Usuarios.findByPk(id).then((usuarioDetalle) => {
+      res.render("detalleusuario", { usuarioDetalle: usuarioDetalle });
+    });
+  },
 
+  createUsuario: (req, res) => {
+    res.render("register");
+  },
 
-     
-}
+  guardarUsuario: (req, res) => {
+    let errors = validationResult(req);
+    if (errors.isEmpty()) {
+      if (req.file) {
+        dbp.Usuarios.create({
+          ...req.body,
+          imagen: req.file.filename,
+          contraseña: req.body.contrasena,
+        });
 
+        res.redirect("/");
+      }
+    } else {
+      res.render("register", { errors: errors.mapped(0) });
+    }
+  },
 
-}
-
-
-   
-
-
-
-
-
-module.exports = controller
-
+  eliminarUsuario: (req, res) => {
+    const users = db.getAllUsers();
+    const id = req.params.id;
+    usersFilter = users.filter((user) => user.id != id);
+    db.writeAndSaveUser(usersFilter);
+    res.redirect("/userList");
+  },
+  editUser: (req, res) => {
+    let id = req.params.id;
+    dbp.Usuarios.findByPk(id).then((userToEdit) => {
+      res.render("editUser", { userToEdit: userToEdit });
+    });
+  },
+  userEdited: (req, res) => {
+    dbp.Usuarios.update(
+      {
+        ...req.body,
+      },
+      {
+        where: { idUsuario: req.params.id },
+      }
+    ).then((usuarioEdited) => {
+      res.redirect("/userList");
+    });
+  },
+};
 
 module.exports = controller;
