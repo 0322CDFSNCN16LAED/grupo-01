49cfeae801e89fdb78c885cd200423ade7e9ac48
LabaquiJose <jose.labaqui@hotmail.com>
2022-09-12 15:34:37 -0300

    inicializando react

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "danidani",
+    "password": "",
     "database": "green-meat",
     "host": "127.0.0.1",
     "dialect": "mysql"
